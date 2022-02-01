3e7cef20accd0f31a2a94ffb2dffca79252d5e44
matt-021 <550369801@qq.com>
2022-02-01 16:53:58 +0800

    项目初始化

---
+++ gulimall-coupon/src/main/java/com/matt/gulimall/gulimattcoupon/GulimattCouponApplication.java
@@ -0,0 +1,13 @@
+package com.matt.gulimall.gulimattcoupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimattCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimattCouponApplication.class, args);
+    }
+
+}
